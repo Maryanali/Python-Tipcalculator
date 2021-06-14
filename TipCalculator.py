f1a986e45b4bc197cb72b1414a9693480f1b9fb0
Maryanali <73916090+Maryanali@users.noreply.github.com>
2021-06-14 11:48:11 +0100

    Initial commit

---
+++ TipCalculator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+#creating a tip calator that allows us to choose what percentage tip we would like to give
+print("Welcome to the tip calculator 😄")
+total_bill = float(input("What is the total bill? £"))
+tip=int(input("What percentage bill would you like to give: 10, 12, or 15? "))
+people= int(input("How many people to split the bill? "))
+total_bill_float= round(float(total_bill),2)
+
+bill_with_tip= round((tip /100 * total_bill + total_bill),2)
+bill_per_person = round((bill_with_tip / people),2)
+print(f"Your bill before tip is £{total_bill_float}, your bill with tip is £{bill_with_tip}, Your bill divided by {people} people is £{bill_per_person}")
+
+
